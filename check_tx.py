5fc2fd3f18a9261e507ada162ce385ab4ef63623
AdiYoOdi <120572211+AdiYoOdi@users.noreply.github.com>
2022-12-15 15:25:43 +0200

    Update check_tx.py

--- check_tx.py
+++ check_tx.py
@@ -3,6 +3,7 @@ import json
 import pygsheets
 import logging
 import pandas as pd
+import time
 
 logging.basicConfig(level=logging.INFO)
 hydra_decimals = 100000000
@@ -11,63 +12,53 @@ def get_hydra_decimal(hydra_value):
     return int(hydra_value)/hydra_decimals
 
 def check_txs(current_transaction):
-    json_response = []
-    #for i in range(len(current_transaction)):
-    endpoint = f"https://explorer.hydrachain.org/api/tx/{current_transaction}" #don't forget [i] for loops
-    headers = {'User-Agent': '...', 'referer': 'https://...'}
-    response = requests.get(endpoint, headers=headers)
-    json_response_loop = json.loads(response.text)
-    outputs = json_response_loop["outputs"]
+    # json_response = []
+    for i in range(len(current_transaction)):
+        endpoint = f"https://explorer.hydrachain.org/api/tx/{current_transaction[i]}" #don't forget [i] for loops
+        headers = {'User-Agent': '...', 'referer': 'https://...'}
+        response = requests.get(endpoint, headers=headers)
+        json_response_loop = json.loads(response.text)
+        outputs = json_response_loop["outputs"]
 
-    block_reward = abs(get_hydra_decimal(json_response_loop["fees"]))
-        
-    if len(outputs) > 3 : # This is a block won by delegator but has other transactions on it , but there are those cases where extra txs are present but the SuperStaker  won so we need to figure out how to identify that case and discard them 
-        delegator = json_response_loop["outputs"][2]
-        other_tx = json_response_loop["outputs"]
-        
-        other_values = []
-        for values in other_tx:
-            v = int(values["value"])
-            other_values.append(v)
-        print(other_values[2])
-        
-        check_number = 500000000
-
-        if other_values[2] > check_number:
-            other_values_cut = other_values[2:]
-            other_values_total = get_hydra_decimal(sum(other_values_cut))
-            delegator_reward = get_hydra_decimal(delegator["value"])
-            delegator_address = delegator["address"]
-            super_staker_fee = block_reward - other_values_total
-            print(delegator_address)
-            print(delegator_reward)
-            print(super_staker_fee)
-            print("output 2")
+        block_reward = abs(get_hydra_decimal(json_response_loop["fees"]))
             
-        else:
-            correct_address_is_staker = json_response_loop["outputs"][1]
-            correct_address = correct_address_is_staker["address"]
-            super_staker_fee = block_reward
-            print(correct_address)
-            print(super_staker_fee)
-            print("output 1")
+        if len(outputs) > 3 : # This is a block won by delegator but has other transactions on it , but there are those cases where extra txs are present but the SuperStaker  won so we need to figure out how to identify that case and discard them 
+            delegator = json_response_loop["outputs"][2]
+            other_tx = json_response_loop["outputs"]
+            
+            other_values = []
+            for values in other_tx:
+                v = int(values["value"])
+                other_values.append(v)
+                    
+            check_number = 500000000
 
+            dict_delegator = {"Address":[], "Reward":[], "Fee":[] }
+            if other_values[2] > check_number:
+                other_values_cut = other_values[2:]
+                other_values_total = get_hydra_decimal(sum(other_values_cut))
+                delegator_reward = get_hydra_decimal(delegator["value"])
+                delegator_address = delegator["address"]
+                super_staker_fee = block_reward - other_values_total
+                dict_delegator["Address"].append(delegator_address)
+                dict_delegator["Reward"].append(delegator_reward)
+                dict_delegator["Fee"].append(super_staker_fee)
+                # print(delegator_address)
+                # print(delegator_reward)
+                # print(super_staker_fee)
+                # print("output 2")
+            
+            else:
+                correct_address_is_staker = json_response_loop["outputs"][1]
+                correct_address = correct_address_is_staker["address"]
+                super_staker_fee = block_reward
+                dict_delegator["Address"].append(correct_address)
+                dict_delegator["Fee"].append(super_staker_fee)
+                # print(correct_address)
+                # print(super_staker_fee)
+                # print("output 1")
+            print(dict_delegator)
 
-    if len(outputs)  == 2:  # This is a block won by super staker 
-        super_staker = json_response_loop["outputs"][1]
-        super_staker_address = super_staker["address"]
-        super_staker_reward = block_reward
-        print(super_staker_address)
-        print(super_staker_reward)
-
-    if len(outputs) == 3: # This is a block won by delegator 
-        delegator_long_tx = json_response_loop["outputs"][2]
-        delegator_long_tx_reward = get_hydra_decimal(delegator_long_tx["value"])
-        delegator_long_tx_address = delegator_long_tx["address"]
-        super_staker_fee = block_reward - delegator_long_tx_reward
-        print(delegator_long_tx_address)
-        print(delegator_long_tx_reward)
-        print(super_staker_fee)
 
 def get_tx_id():
     wallet_address = "HM6TFbbBvrJ4hcMTFT93R7DBz1FmcUTGAC"
@@ -83,8 +74,10 @@ def get_tx_id():
         tx_ids.append(tx_id)
     return tx_ids
 
+all_txs = get_tx_id()
+check_txs(current_transaction=all_txs)
 
-check_txs(current_transaction="e2f5c46868cf77b47f5376817a637f85d06baa697f49c8b2f4131be6dc590c4e")
+# check_txs(current_transaction="55b2f7fde386f31e76ede065320df742c70ae92f7a09d1e78d79f8a384fda3f1")
 
 """ 
 I identified 4 situations for mining blocks transactions  :
