90bf1b3b693fa5767ee9aea8ebeb10dc3357accf
Adi <120572211+AdiYoOdi@users.noreply.github.com>
2022-12-18 20:34:03 +0200

    Final version ... for now
    It gets all the data from Stray wallet ... I filtered out the blocks before super staking. There are still a few blocks that i need to filter out , the ones where there's a transaction on the wallet in/out. The solution would be to check [0] in output if its not 0 that means that's a "bad" block. 
    
    For now i filter those "bad" blocks in google sheets

--- check_tx.py
+++ check_tx.py
@@ -3,18 +3,20 @@ import json
 import pygsheets
 import logging
 import pandas as pd
-import numpy as np
-import openpyxl
+
 
 logging.basicConfig(level=logging.INFO)
 hydra_decimals = 100000000
 
+
 def get_hydra_decimal(hydra_value):
     return int(hydra_value)/hydra_decimals
 
+
 def check_txs(current_transaction):
+    final_results = []
     for i in range(len(current_transaction)):
-        endpoint = f"https://explorer.hydrachain.org/api/tx/{current_transaction[i]}" #don't forget [i] for loops
+        endpoint = f"https://explorer.hydrachain.org/api/tx/{current_transaction[i]}"  # don't forget [i] for loops
         headers = {'User-Agent': '...', 'referer': 'https://...'}
         response = requests.get(endpoint, headers=headers)
         json_response_loop = json.loads(response.text)        
@@ -22,11 +24,10 @@ def check_txs(current_transaction):
         outputs = json_response_loop["outputs"]
         # outputs_zero = json_response_loop["outputs"][0].get("value") #For future use to check if [0]value == None 
 
-
         block_reward = abs(get_hydra_decimal(json_response_loop["fees"]))
 
         dict_delegator = []
-        if len(outputs) > 3 : # This is a block won by delegator but has other transactions on it , but there are those cases where extra txs are present but the SuperStaker  won so we need to figure out how to identify that case and discard them 
+        if len(outputs) > 3:  # This is a block won by delegator but has other transactions on it , but there are those cases where extra txs are present but the SuperStaker  won, so we need to figure out how to identify that case and discard them
             delegator = json_response_loop["outputs"][2]
             other_tx = json_response_loop["outputs"]
             
@@ -51,22 +52,21 @@ def check_txs(current_transaction):
                 correct_address_is_staker = json_response_loop["outputs"][1]
                 correct_address = correct_address_is_staker["address"]
                 super_staker_fee = block_reward
-                delegator_reward = int(0)
+                delegator_reward = 0
                 dict_delegator.append(correct_address)                
                 dict_delegator.append(super_staker_fee)
                 dict_delegator.append(delegator_reward)
 
-
-        if len(outputs)  == 2:  # This is a block won by super staker 
+        if len(outputs) == 2:  # This is a block won by super staker
             super_staker = json_response_loop["outputs"][1]
             super_staker_address = super_staker["address"]
             super_staker_reward = block_reward
-            super_staker_fee = "0"
+            super_staker_fee = 0
             dict_delegator.append(super_staker_address)
             dict_delegator.append(super_staker_reward)
             dict_delegator.append(super_staker_fee)
 
-        if len(outputs) == 3: # This is a block won by delegator 
+        if len(outputs) == 3:  # This is a block won by delegator
             delegator_long_tx = json_response_loop["outputs"][2]
             delegator_long_tx_reward = get_hydra_decimal(delegator_long_tx["value"])
             delegator_long_tx_address = delegator_long_tx["address"]
@@ -74,24 +74,11 @@ def check_txs(current_transaction):
             dict_delegator.append(delegator_long_tx_address)
             dict_delegator.append(delegator_long_tx_reward)
             dict_delegator.append(super_staker_fee)
-        
-        # K = 51
-        # dict_delegator_trunc = dict_delegator[:-K]
-        
-        
-        path = 'straypet-45528403089c.json'
 
-        gc = pygsheets.authorize(service_account_file=path)
+        results = dict_delegator
+        final_results.append(results)
 
-        sh = gc.open('Math')
-
-        wk1 = sh[0]
-        wk1.append_table(values = dict_delegator, start = 'A2', end = None, dimension = 'ROWS', overwrite = True)
-
-        # wk1.insert_rows(row=wk1.rows, number=1, values=dict_delegator)
-
-        
-
+    return final_results
 
 
 def get_tx_id():
@@ -101,47 +88,23 @@ def get_tx_id():
     response = requests.get(endpoint, headers=headers)
     json_response = json.loads(response.text)
     tx_transactions = json_response["transactions"]
-    # Get data from [{}] type of json : array of objects i think is called:
+    # Get data from [{}] type of json : array of objects I think is called:
     tx_ids = []
-    for id in tx_transactions:
-        tx_id = id["id"]
+    for ids in tx_transactions:
+        tx_id = ids["id"]
         tx_ids.append(tx_id)
     return tx_ids
 
+
 all_txs = get_tx_id()
-check_txs(current_transaction=all_txs)
-# check_txs(current_transaction="e2f5c46868cf77b47f5376817a637f85d06baa697f49c8b2f4131be6dc590c4e")
+result = check_txs(all_txs)
+result = result[:-51]
+print(result)
 
-""" 
-I identified 4 situations for mining blocks transactions  :
-    1. Super Staker won the block an easy transaction where there are only 2 results in  outputs:
-        https://explorer.hydrachain.org/tx/e2f5c46868cf77b47f5376817a637f85d06baa697f49c8b2f4131be6dc590c4e
-            output[0] is always empty 
-            output[1] is always your own wallet address but the Hydra near it is not the reward but the utxo used in tx : 
-                [outputs][2][value] /= block reward
-                block reward = main_json_response["fees"]
 
-    2. Super Staker won the block but there are other transactions , so outputs[2] = superstaker and i check if outputs[3][value] < 5Hydra
-        https://explorer.hydrachain.org/tx/55b2f7fde386f31e76ede065320df742c70ae92f7a09d1e78d79f8a384fda3f1
-            output[0] is always empty
-            output[1] is always your own wallet address
-            output[2] is not the delegator in this situation, because the hydra ["value"] = 0.29639692 HYDRA so thats not a reward for staking
-                So i filter results with if statments if value <5H is not delegator  and i say SuperSTaker won 
-
-    3. Delegator won the block, only 3 outputs and we check if output[2]< than 5 Hydra :
-        https://explorer.hydrachain.org/tx/117e81f24ba7149acea0cd3c159ab0a2202bb0a2d8176c77f81216d0ec17761f
-            output[0] is always empty
-            output[1] is always your own wallet address
-            output[2] is the Delegator
-            
-    4. Delegator won the block , there are multiple outputs:
-        https://explorer.hydrachain.org/tx/34023cc636a3256c5b1a1b3cea60c21d9cac49457c70a559f67116bcc3f14ccf
-            output[0] is always empty
-            output[1] is always your own wallet address
-            output[2] is the Delegator
-            output[3 4 5 ] extra fluf we need to discard
-    
-remove last 51 transactions to do 
-
-"""
-
+path = 'straypet-45528403089c.json'
+gc = pygsheets.authorize(service_account_file=path)
+sh = gc.open('Math')
+wk1 = sh[1]
+wk1.clear(start = 'A2')
+wk1.append_table(values = result, start = 'A2', end = None, dimension = 'ROWS', overwrite = True)
